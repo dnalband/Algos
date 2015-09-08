d81033479ba2c4e399531751c0ca536d31c63969
dnalband <dpeers@gmail.com>
2015-09-08 21:29:36 +0530

    largest sub array - Kadane's algo and chek pairs for a specific sum

---
+++ arrays/src/main/java/com/dada/CheckPair.java
@@ -0,0 +1,77 @@
+package com.dada;
+
+/**
+ * Created by dnalband on 31/08/15.
+ */
+public class CheckPair {
+
+   static int partition(int a[], int l , int r)
+    {
+          int p = a[r];
+
+        int i = l -1 ;
+        int temp = 0;
+        for (int j =l; j < a.length-1; j++)
+        {
+              if(a[j] < a[r])
+              {
+                  i++;
+                   temp = a[i];
+                  a[i] = a[j];
+                  a[j] = temp;
+              }
+        }
+
+        temp = a[i+1];
+        a[i+1] = a[r];
+        a[r] = temp;
+        return i+1;
+    }
+
+    static void qsort(int[] a, int l , int r)
+    {
+        if(l < r)
+        {
+            int p = partition(a, l, r);
+            qsort(a, l , p-1);
+            qsort(a, p+1, r);
+        }
+    }
+
+    static int checkPairForSum(int[] a, int sum)
+    {
+        int l = 0;
+        int r = a.length-1;
+
+        while(l < r)
+        {
+            if(a[l]+a[r] == sum )
+            {
+                System.out.println("pair for sum "+ sum+ " are "+ a[l]+", "+a[r]);
+               l++; r--;  // this is to find number of pairs
+            }
+
+            else if(a[l]+a[r] < sum)
+            {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        int [] a = {12,2,4,10,6,8,9,54};
+
+         qsort(a, 0, a.length-1);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        checkPairForSum(a, 14);
+
+    }
+}
---
+++ arrays/src/main/java/com/dada/LargestSum.java
@@ -0,0 +1,46 @@
+package com.dada;
+
+/**
+ * Created by dnalband on 08/09/15.
+ *
+ * very good problem, just check for start index and last index for finding max sub array
+ */
+public class LargestSum {
+
+   static int max(int x, int y)
+   {
+       return x > y ? x : y;
+   }
+    static int[] largestSum(int [] array)
+    {
+
+        int current_sum = 0;
+        int sum_so_far = 0;
+        int [] largestSum = new int[3];
+        int tempStartIndex =0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            current_sum = current_sum+array[i];
+            if (current_sum < 0)
+            {
+                current_sum =0;
+                tempStartIndex = i+1;
+
+            }
+            if (current_sum > sum_so_far)
+            {
+                sum_so_far = current_sum;
+                largestSum[0] = tempStartIndex;
+                largestSum[1] = i;
+                largestSum[2] = sum_so_far;
+            }
+        }
+        return largestSum;
+    }
+
+    public static void main(String[] args) {
+        int [] array = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(largestSum(array)[1]);
+    }
+}
